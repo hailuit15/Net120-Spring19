1fd4846c707799d7140518b3728c3ea934d92d8e
Hailu Abera <hailemariam.abera@gmail.com>
2019-06-06 09:36:49 -0700

    Add files via upload

---
+++ Net120_Python.py
@@ -0,0 +1,11 @@
+print("This is a Net120 Course 'Hello World' Python Assignement")
+
+print ("Hello World")
+
+print("To Professor Messerly: Thank you for the entire lecture section you delivered us")
+print("It was a great experience for me to have that class")
+
+
+input("Press any ket to exit")
+
+ 
